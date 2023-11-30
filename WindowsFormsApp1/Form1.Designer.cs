5d2d7499ac2fa395174abbb5db000ad1a47eedc1
w-modz <46196436+w-modz@users.noreply.github.com>
2023-11-30 15:34:17 +0100

    Rename Form1

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -2,14 +2,8 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
@@ -22,10 +16,8 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.InputLabel = new System.Windows.Forms.Label();
@@ -126,7 +118,7 @@
             this.Controls.Add(this.InputTextbox);
             this.Controls.Add(this.InputLabel);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "CSV->HTML Table Converter";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -9,9 +9,7 @@ namespace WindowsFormsApp1
 {
     internal static class Program
     {
-        /// <summary>
         /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
